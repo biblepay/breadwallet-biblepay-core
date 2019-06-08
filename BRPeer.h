919cf9688b7fa1d427555f0b7739e9d08601f079
MIP <mobile@biblepay.org>
2019-06-08 22:12:20 +0200

    BBP Evo parameters

--- BRPeer.h
+++ BRPeer.h
@@ -55,9 +55,9 @@ extern "C" {
 #define SERVICES_NODE_NETWORK 0x01 // services value indicating a node carries full blocks, not just headers
 #define SERVICES_NODE_BLOOM   0x04 // BIP111: https://github.com/bitcoin/bips/blob/master/bip-0111.mediawiki
 #define SERVICES_NODE_BCASH   0x20 // https://github.com/Bitcoin-UAHF/spec/blob/master/uahf-technical-spec.md
-    
-#define BR_VERSION "2.1"
-#define USER_AGENT "/bread:" BR_VERSION "/"
+
+#define BR_VERSION "1.4.3.3"
+#define USER_AGENT "/Main:" BR_VERSION "/"
 
 // explanation of message types at: https://en.bitcoin.it/wiki/Protocol_specification
 #define MSG_VERSION     "version"
